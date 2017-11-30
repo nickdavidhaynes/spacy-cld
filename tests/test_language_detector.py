10e20b9b073df05bad6c833566387f8d5af17c73
Nick Haynes <nickdavidhaynes@gmail.com>
2017-11-29 20:47:27 -0500

    Make tests slightly easier to run

--- tests/test_language_detector.py
+++ tests/test_language_detector.py
@@ -4,7 +4,7 @@ from spacy_cld import LanguageDetector
 
 def test_language_detector():
     nlp = spacy.load('en')
-    language_detector = LanguageDetector(nlp)
+    language_detector = LanguageDetector()
     nlp.add_pipe(language_detector)
     doc = nlp('This is some English text.')
     assert len(doc._.languages) > 0
