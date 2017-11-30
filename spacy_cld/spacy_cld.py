045848b1f7740de82ce2825c09a1437be189bd4b
Nick Haynes <nickdavidhaynes@gmail.com>
2017-11-29 22:23:46 -0500

    Refactor for cleanliness and an easier API.

--- spacy_cld/spacy_cld.py
+++ spacy_cld/spacy_cld.py
@@ -1,29 +1,39 @@
-from __future__ import unicode_literals
 from pycld2 import detect
 from spacy.tokens import Doc, Span
+
+
+def get_languages(text, cld_results=None):
+    if cld_results is None:
+        cld_results = detect_languages(text)
+    return [lang for (_, lang, _, _) in cld_results if lang != 'Unknown']
+
+
+def get_scores(text, cld_results=None):
+    if cld_results is None:
+        cld_results = detect_languages(text)
+    return {lang: score / 100 for (_, lang, score, _) in cld_results}
+
+
+def detect_languages(text):
+    _, _, results = detect(text.text)
+    return results
 
 
 class LanguageDetector(object):
 
     name = 'cld'
 
-    def __init__(self, attrs=('languages')):
-        _detect_languages = attrs
-        Doc.set_extension(_detect_languages, getter=self.detect_languages)
-        Span.set_extension(_detect_languages, getter=self.detect_languages)
+    def __init__(self, attrs=('languages', 'language_scores')):
+        self._languages, self._scores = attrs
+        Doc.set_extension(self._languages, getter=get_languages)
+        Doc.set_extension(self._scores, getter=get_scores)
+        Span.set_extension(self._languages, getter=get_languages)
+        Span.set_extension(self._scores, getter=get_scores)
 
     def __call__(self, doc):
         '''Apply the language detector as a pipeline component.'''
-        doc._.languages = self.detect_languages(doc)
+        # Make a single call to the language detector and cache the result.
+        cld_results = detect_languages(doc)
+        doc._.set(self._languages, get_languages(doc, cld_results))
+        doc._.set(self._scores, get_scores(doc, cld_results))
         return doc
-
-    def detect_languages(self, text):
-        '''Thin wrapper around pycld2 API, which itself is a thin wrapper
-        around the original CLD library.'''
-        _, _, results = detect(text.text)
-        languages = {}
-        for (name, code, confidence, _) in results:
-            if name != 'Unknown':
-                languages[code] = {'name': name,
-                                   'confidence': confidence / 100}
-        return languages
